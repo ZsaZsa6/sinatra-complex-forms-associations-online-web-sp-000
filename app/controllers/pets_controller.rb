6c61b7238d8a0bb0fe91579610de069190aec195
Ronda D Cowen <cowenhome5@gmail.com>
2020-06-02 16:19:33 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,7 +26,8 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
 
